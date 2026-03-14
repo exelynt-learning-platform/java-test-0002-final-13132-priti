1d154483008839f6de093cdd8ae4fee88a59cd66
Priti Pansare <129170850+pansare583@users.noreply.github.com>
2026-03-14 11:17:29 +0530

    Add pattern_prog2.java to print number patterns

---
+++ pattern_prog2.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+
+            // Print increasing numbers
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+
+            // Print decreasing numbers
+            for (int j = i - 1; j >= 1; j--) {
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
+    }
+}
