7100e027d26a1aa8eef76a94edebcf65388efc79
Priti Pansare <129170850+pansare583@users.noreply.github.com>
2026-03-14 13:44:10 +0530

    Update Pattern_prog2.java
    Updated the class declaration to include a package statement and improved code formatting.

--- Pattern_prog2.java
+++ Pattern_prog2.java
@@ -1,16 +1,21 @@
-public class Pattern_prog2{
+package priti;
+public class Pattern_prog2 {
     public static void main(String[] args) {
 
         int n = 5;
 
         for (int i = 1; i <= n; i++) {
 
-            // Print increasing numbers
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            
             for (int j = 1; j <= i; j++) {
                 System.out.print(j);
             }
 
-            // Print decreasing numbers
+            
             for (int j = i - 1; j >= 1; j--) {
                 System.out.print(j);
             }
